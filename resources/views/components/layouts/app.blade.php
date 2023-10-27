d966d98119c14b59f7aff16c4ecd4e20b15ffa0f
feriwnarta <feriwnarta26@gmail.com>
2023-10-27 14:04:13 +0700

    add gate super admin laravel spatie

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -21,7 +21,7 @@
 
         <script src="{{ asset('js/jquery.js') }}"></script>
         <script src="{{ asset('data-table/datatables.js') }}"></script>
-        <script src="{{ asset('js/toast.js') }}"></script>
+{{--        <script src="{{ asset('js/toast.js') }}"></script>--}}
         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
                 integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous">
         </script>
