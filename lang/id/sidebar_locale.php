bad73ec073ac8b8ef6d5bac0c9333abc2ce5de41
feriwnarta <feriwnarta26@gmail.com>
2023-10-27 14:15:38 +0700

    add locale sidebar indonesia

---
+++ lang/id/sidebar_locale.php
@@ -0,0 +1,49 @@
+<?php
+
+return [
+    'pos' => [],
+    'gudang' => [
+        'stokItem' => 'Stok item',
+        'daftarGudang' => 'Daftar gudang',
+        'kategoriItem' => 'Kategori Item',
+        'unit' => 'Unit',
+        'itemKeluar' => 'Item keluar',
+        'itemMasu' => 'Item masuk',
+        'memintaPembelian' => 'Meminta pembelian',
+        'permintaanProduksi' => 'Permintaan produksi',
+        'permintaanStok' => 'Permintaan Stok',
+        'laporanGudang' => 'Laporan gudang',
+    ],
+    'dapurSentral' => [
+
+    ],
+    'outlet' => [
+
+    ],
+    'pembelian' => [
+
+    ],
+    'akuntansi' => [
+
+    ],
+
+    'finansial' => [
+
+    ],
+
+    'user' => [
+
+    ],
+
+    'notifikasi' => [
+
+    ],
+
+    'bantuan' => [
+
+    ],
+
+    'pengaturan' => [
+
+    ],
+];
