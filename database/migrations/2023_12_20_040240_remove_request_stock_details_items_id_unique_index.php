78e87cef5567727c2c95f86b9f2f9596861055f4
feriwnarta <feriwnarta26@gmail.com>
2023-12-20 11:07:30 +0700

    fix migrate

--- database/migrations/2023_12_20_040240_remove_request_stock_details_items_id_unique_index.php
+++ database/migrations/2023_12_20_040240_remove_request_stock_details_items_id_unique_index.php
@@ -11,6 +11,8 @@ return new class extends Migration {
     {
         Schema::table('request_stock_details', function (Blueprint $table) {
             $table->dropForeign('request_stock_details_items_id_foreign');
+            $table->dropUnique('request_stock_details_items_id_unique');
+            $table->foreign('items_id')->references('id')->on('items');
         });
     }
 
@@ -21,6 +23,7 @@ return new class extends Migration {
     {
         Schema::table('request_stock_details', function (Blueprint $table) {
             $table->foreign('items_id')->references('id')->on('items');
+            $table->unique('items_id');
         });
     }
 };
