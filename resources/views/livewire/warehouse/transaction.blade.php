748f5ac3e6c11dec7e1236e4a4ae732d373f0526
feriwnarta <feriwnarta26@gmail.com>
2024-01-10 10:46:10 +0700

    update table stok masuk pakai relasi morp

--- resources/views/livewire/warehouse/transaction.blade.php
+++ resources/views/livewire/warehouse/transaction.blade.php
@@ -122,9 +122,10 @@
 
                         @foreach($requestStock as $request)
                             <tr class="items-table-head-color" id="po1" style="cursor: pointer">
-                                <td>{{ $request->code ?? ' - ' }}</td>
-                                <td>{{Log::info( ($request->reference)) }}</td>
-
+                                <td>{{ $request->itemReceipt->code ?? ' - ' }}</td>
+                                <td>{{ $request->receivable->code }}</td>
+                                <td> {{ Carbon::createFromFormat('Y-m-d H:i:s', $request->created_at)->locale('id_ID')->isoFormat('D MMMM Y') }}</td>
+                                <td>{{ $request->itemReceipt->history->last()->status }}</td>
                             </tr>
 
                         @endforeach
